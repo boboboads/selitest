fc7783fbb31c773c907ba0ede62c97d9ea00302c
boboboads <dinaradagieva243@gmail.com>
2025-12-13 14:58:50 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,12 @@
-if requesto then
-  print('there is requesto')
+print('loaded seli controller')
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
+local ls = player:WaitForChild("leaderstats")
+local rebirths = ls:WaitForChild("Rebirths")
+
+if rebirths.Value > 0 then 
+    print('loading bopper')
+    loadstring(fetch("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
 else
-  print('nah no requesto')
+    loadstring(fetch("https://raw.githubusercontent.com/sigmaboiethena/autoreb2/refs/heads/main/main.lua"))()
 end
