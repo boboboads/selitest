061a299e9b8fac7207acd606c3a9976ad2b82392
boboboads <dinaradagieva243@gmail.com>
2026-02-07 16:15:25 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,10 +3,13 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
 local ls = player:WaitForChild("leaderstats")
 local rebirths = ls:WaitForChild("Rebirths")
+local TeleportService = game:GetService("TeleportService")
+pcall(TeleportService.SetTeleportGui, TeleportService, workspace)
 
-if rebirths.Value > 0 then 
-    print('loading bopper')
-    loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
-else
-    loadstring(loadreq("https://raw.githubusercontent.com/boboboads/phoenix/refs/heads/main/main.lua"))()
-end
+loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
+-- if rebirths.Value > 0 then 
+--     print('loading bopper')
+--     loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
+-- else
+--     loadstring(loadreq("https://raw.githubusercontent.com/boboboads/phoenix/refs/heads/main/main.lua"))()
+-- end
