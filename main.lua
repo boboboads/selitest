cdab9293922b96c52c9f77d65a997f15470ea7dc
boboboads <dinaradagieva243@gmail.com>
2026-05-01 23:17:03 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,19 +3,19 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
 local ls = player:WaitForChild("leaderstats")
 local rebirths = ls:WaitForChild("Rebirths")
-local TeleportService = game:GetService("TeleportService")
-pcall(TeleportService.SetTeleportGui, TeleportService, workspace)
+--local TeleportService = game:GetService("TeleportService")
 
 --loadstring(loadreq("https://raw.githubusercontent.com/boboboads/whopper/refs/heads/main/main.lua"))()
-local chance = math.random(1, 30)
-if chance < 39 then 
-  loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
-else
-  loadstring(loadreq("https://raw.githubusercontent.com/boboboads/nopper/refs/heads/main/main.lua"))()
-end
--- if rebirths.Value > 0 then 
---     print('loading bopper')
---     loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
+-- local chance = math.random(1, 30)
+-- if chance < 39 then 
+--   loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
 -- else
---     loadstring(loadreq("https://raw.githubusercontent.com/boboboads/phoenix/refs/heads/main/main.lua"))()
+--   loadstring(loadreq("https://raw.githubusercontent.com/boboboads/nopper/refs/heads/main/main.lua"))()
 -- end
+if rebirths.Value > 0 then 
+    print('loading bopper')
+    loadstring(loadreq("https://raw.githubusercontent.com/boboboads/bopper/refs/heads/main/main.lua"))()
+else
+    print('loading phoenix')
+    -- loadstring(loadreq("https://raw.githubusercontent.com/boboboads/phoenix/refs/heads/main/main.lua"))()
+end
